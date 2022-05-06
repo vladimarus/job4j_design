49aa8b974dfd108b18b700f254ce2551391ff2a0
vladimarus <cyberstalker@ya.ru>
2022-05-06 23:30:41 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -0,0 +1,54 @@
+package ru.job4j.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Пример использования обобщений (<generics>) 3-мя способами:
+ * 1. Wildcard - подстановка без ограничений (<?>)
+ * 2. Bounded Wildcard - подстановка с ограничением сверху (<? Extends Predator>)
+ * 3. Lower bounded Wildcard - подстановка с ограничением снизу (<? Super Predator>)
+ */
+public class Generics {
+    public static void main(String[] args) {
+        Generics gen = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
+
+        gen.printObject(first);
+        gen.printObject(second);
+        gen.printObject(third);
+        System.out.println();
+
+//        gen.printBoundedWildCard(first);
+        gen.printBoundedWildCard(second);
+        gen.printBoundedWildCard(third);
+        System.out.println();
+
+        gen.printLowerBoundedWildCard(first);
+        gen.printLowerBoundedWildCard(second);
+//        gen.printLowerBoundedWildCard(third);
+    }
+
+    public void printObject(List<?> list) {
+        for (Object next : list) {
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Object next : list) {
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
+        for (Object next : list) {
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+}
