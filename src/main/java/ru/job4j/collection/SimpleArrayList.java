c86e7dbdb24f5864835a30e7a8a013fa6a20da8f
vladimarus <cyberstalker@ya.ru>
2022-06-06 16:52:50 +0600

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,86 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            extendContainer();
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        T oldValue = container[index];
+        container[index] = newValue;
+        modCount++;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T extracted = container[index];
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        size--;
+        container[size] = null;
+        modCount++;
+        return extracted;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<>() {
+            final int expectedModCount = modCount;
+            int itdex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return itdex < size;
+            }
+
+            @Override
+            public T next() {
+                if (!this.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return container[itdex++];
+            }
+        };
+    }
+
+    private void extendContainer() {
+        container = Arrays.copyOf(container, container.length * 2);
+    }
+}
+
